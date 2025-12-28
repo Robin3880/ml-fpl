ba1c12cc50bac1262d899b2d838998ebd9505814
Robin3880 <robinkonrad101@gmail.com>
2025-12-28 14:00:17 +0000

    added fetching defensive data 2024-2025

--- app/fetch_historical_data.py
+++ app/fetch_historical_data.py
@@ -39,7 +39,22 @@ for season in seasons:
         f.write(fixtures_response.text)
 
 
-            
+# the vaastav github repo does not have defensive CBIT stats which are needed for the new 25/26 defcon points rules so will fetch this from a different github repo
+# (defenders with with at least 10 CBIT get 2 extra points,  midfielders with at least 12 CBIRT get 2 extra points)            
+# CBIT - clearances blocks interecpetions tackles
+# CBIRT - clearances blocks interceptions recoveries tackles
 
+# data only avaialable for 2024-2025 season,    will use this to train a seperate defensive contribution pts predictor model
+defensive_dir = os.path.join(root_dir, "data")
+if not os.path.exists(defensive_dir):
+    os.makedirs(defensive_dir)
+
+url = f"https://raw.githubusercontent.com/olbauday/FPL-Core-Insights/refs/heads/main/data/2024-2025/playermatchstats/playermatchstats.csv"
+
+season_path = os.path.join(defensive_dir, f"2024-2025_defensive.csv")
+
+response = requests.get(url)
+with open(season_path, "w", encoding="utf-8") as f:
+    f.write(response.text)
 
 
