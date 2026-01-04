b03a615f7fd38f5a20406ed0d7a3528ff66b86de
Robin3880 <robinkonrad101@gmail.com>
2026-01-04 16:26:23 +0000

    added codes to teams

--- app/classes/team.py
+++ app/classes/team.py
@@ -5,7 +5,7 @@ class Team():
         self.name = data["name"]
         self.strength = data["strength"] 
         self.elo = data["elo"]
-        
+        self.code = data["code"]
         
     def __str__(self):
         return f"{self.id}, {self.name}, {self.strength}, {self.elo}"
