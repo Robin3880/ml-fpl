c3e3475eb48a99df219a1d193fff4d67ade9e303
Robin3880 <robinkonrad101@gmail.com>
2026-01-06 22:32:41 +0000

    created new Fixture class

---
+++ app/classes/fixture.py
@@ -0,0 +1,8 @@
+class Fixture:
+    def __init__(self, team_strength, opponent_strength, team_elo, opponent_elo, was_home, position):
+        self.team_strength = team_strength
+        self.opponent_strength = opponent_strength
+        self.team_elo = team_elo
+        self.opponent_elo = opponent_elo
+        self.was_home = was_home
+        self.position = position
